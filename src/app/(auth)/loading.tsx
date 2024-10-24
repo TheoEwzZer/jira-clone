dd4d7926fad19d701a4cbb84830e16231c2a819a
Théo <theo.fabianomattei@gmail.com>
2024-10-24 15:18:32 +0200

    fix: update page layouts to use full height

--- src/app/(auth)/loading.tsx
+++ src/app/(auth)/loading.tsx
@@ -5,7 +5,7 @@ import { ReactElement } from "react";
 
 const LoadingPage: () => ReactElement = (): ReactElement => {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex h-full flex-col items-center justify-center">
       <Loader className="size-6 animate-spin text-muted-foreground" />
     </div>
   );
